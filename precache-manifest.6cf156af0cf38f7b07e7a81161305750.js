36945ab108309f9bf568ebe3a6dea2ec6e4b698e
sayma@newgen-bd.com <turzoxpress@gmail.com>
2020-10-05 10:32:19 +0600

    ip changed

---
+++ precache-manifest.6cf156af0cf38f7b07e7a81161305750.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb53ec8edeae3395b5f9a54424ea1666",
+    "url": "/lms/index.html"
+  },
+  {
+    "revision": "2191f3f66caab78e6f9c",
+    "url": "/lms/static/css/2.d195aa5e.chunk.css"
+  },
+  {
+    "revision": "429c53c036529d38913d",
+    "url": "/lms/static/css/main.df35e8dd.chunk.css"
+  },
+  {
+    "revision": "2191f3f66caab78e6f9c",
+    "url": "/lms/static/js/2.46031505.chunk.js"
+  },
+  {
+    "revision": "f6a33b08b8dab27dd8ab525dc9994832",
+    "url": "/lms/static/js/2.46031505.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "429c53c036529d38913d",
+    "url": "/lms/static/js/main.15dd7c8d.chunk.js"
+  },
+  {
+    "revision": "d97a0ffe98da858c4cca",
+    "url": "/lms/static/js/runtime-main.46850f1d.js"
+  }
+]);
