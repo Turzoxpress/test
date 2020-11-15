b0270d189f319cae194ae4f431b97013dda5b843
sayma@newgen-bd.com <turzoxpress@gmail.com>
2020-11-15 17:55:42 +0600

    11-15-2020

---
+++ precache-manifest.6f548cf75bf1c1f237248c175f133a82.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f46419cc4e7793bbc1b739cabe9c5277",
+    "url": "/lms/index.html"
+  },
+  {
+    "revision": "ad0a0193c0d8aa6e0959",
+    "url": "/lms/static/css/2.d195aa5e.chunk.css"
+  },
+  {
+    "revision": "ed45eb2d0667d6838667",
+    "url": "/lms/static/css/main.88acfb7f.chunk.css"
+  },
+  {
+    "revision": "ad0a0193c0d8aa6e0959",
+    "url": "/lms/static/js/2.30e9f26a.chunk.js"
+  },
+  {
+    "revision": "aaa14393f466f93b7d0903f67fc3f3f7",
+    "url": "/lms/static/js/2.30e9f26a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed45eb2d0667d6838667",
+    "url": "/lms/static/js/main.484d1277.chunk.js"
+  },
+  {
+    "revision": "d97a0ffe98da858c4cca",
+    "url": "/lms/static/js/runtime-main.46850f1d.js"
+  },
+  {
+    "revision": "9af1038cf8263aae24e0e9a0687d71c3",
+    "url": "/lms/static/media/Logo2.9af1038c.png"
+  },
+  {
+    "revision": "4d66afad915b5fce6f4599b7247f5085",
+    "url": "/lms/static/media/bg.4d66afad.jpg"
+  },
+  {
+    "revision": "f4477b86cdb4e9b83ec7f44dc59ad72c",
+    "url": "/lms/static/media/bg.f4477b86.jpg"
+  },
+  {
+    "revision": "33b242a29eaa55ee57f659787f46295a",
+    "url": "/lms/static/media/bg2.33b242a2.jpg"
+  },
+  {
+    "revision": "b064fe4f2b2c8a5d27b799713dcfb1c8",
+    "url": "/lms/static/media/logo2.b064fe4f.png"
+  }
+]);
