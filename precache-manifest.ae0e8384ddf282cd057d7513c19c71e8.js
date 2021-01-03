68d48e459786e0dcdcce605e814ea0e4b55ad3cc
Turzo <turzoxpress@gmail.com>
2021-01-03 09:58:28 +0600

    2-1-2020

---
+++ precache-manifest.ae0e8384ddf282cd057d7513c19c71e8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4d4d3d0e49c29954e7474a611b3c2d3",
+    "url": "/lms/index.html"
+  },
+  {
+    "revision": "4fc470c04ba6593c2642",
+    "url": "/lms/static/css/2.d195aa5e.chunk.css"
+  },
+  {
+    "revision": "28aaadd39ccdd2810538",
+    "url": "/lms/static/css/main.a631702c.chunk.css"
+  },
+  {
+    "revision": "4fc470c04ba6593c2642",
+    "url": "/lms/static/js/2.94df4d93.chunk.js"
+  },
+  {
+    "revision": "4b0c86ae11b9827c8a604383ad1d6471",
+    "url": "/lms/static/js/2.94df4d93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28aaadd39ccdd2810538",
+    "url": "/lms/static/js/main.7931dd09.chunk.js"
+  },
+  {
+    "revision": "d97a0ffe98da858c4cca",
+    "url": "/lms/static/js/runtime-main.46850f1d.js"
+  },
+  {
+    "revision": "9af1038cf8263aae24e0e9a0687d71c3",
+    "url": "/lms/static/media/Logo2.9af1038c.png"
+  },
+  {
+    "revision": "4d66afad915b5fce6f4599b7247f5085",
+    "url": "/lms/static/media/bg.4d66afad.jpg"
+  },
+  {
+    "revision": "f4477b86cdb4e9b83ec7f44dc59ad72c",
+    "url": "/lms/static/media/bg.f4477b86.jpg"
+  },
+  {
+    "revision": "33b242a29eaa55ee57f659787f46295a",
+    "url": "/lms/static/media/bg2.33b242a2.jpg"
+  },
+  {
+    "revision": "b064fe4f2b2c8a5d27b799713dcfb1c8",
+    "url": "/lms/static/media/logo2.b064fe4f.png"
+  }
+]);
